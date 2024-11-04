6f7bc938551abf57f2e4e3752894a4b2787611d6
Pedro Belém <pedro.belem@ucsal.edu.br>
2024-11-04 19:04:54 -0300

    Add files via upload

---
+++ LearningJavaLPA/src/aplications/Classe26.java
@@ -0,0 +1,36 @@
+package aplications;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Classe26 {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int x = 0;
+		
+		
+		for (int i = 0; i == 0;) {
+
+			System.out.println("Insira um valor maior que 0 e menor que 1000");
+			x = sc.nextInt();
+
+			if (x < 0 || x > 1000) {
+				System.out.println("Valor fora do intervalo solicitado");
+			} else {
+				i++;
+			}
+		}
+		
+		if (x % 2 == 0) {
+			System.out.println("O valor é par");
+		} else {
+			for (int i = 0; i <= x; i++) {
+				System.out.println("O valor impar é: " + i);
+			}
+		}
+		
+		sc.close();
+	}
+}
---
+++ LearningJavaLPA/src/aplications/Classe34.java
@@ -0,0 +1,31 @@
+package aplications;
+
+import java.util.Scanner;
+
+public class Classe34 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Digite a quatidade de números: ");
+		int tamanho = sc.nextInt();
+
+		int[] numeros = new int[tamanho];
+
+		for (int i = 0; i < tamanho; i++) {
+			System.out.print("Digite o número " + (i + 1) + ": ");
+			numeros[i] = sc.nextInt();
+		}
+
+		sc.close();
+
+		int soma = 0;
+		for (int numero : numeros) {
+			soma += numero;
+		}
+
+		System.out.println("A soma dos números é: " + soma);
+	}
+
+}
